9f39175d5c79e7e2d92e398dc5aa8e72a5b2f866
VIVIANA RESTREPO QUINTERO <lvrestrepo89570@umanizales.edu.co>
2022-11-18 19:47:21 -0500

    Se crea el componente  imagen y scrool

--- spaproyectofinal/src/App.js
+++ spaproyectofinal/src/App.js
@@ -5,12 +5,54 @@ class App extends Component {
   //creamos el objeto state
   state = {
     termino : '', 
-    imagenes : []
+    imagenes : [],
+    pagina : ''
+  }
+
+scroll = () => {
+  const elemento = document.querySelector('.jumbotron');
+  elemento.scrollIntoView('smooth', 'start');
+}
+  
+  paginaAnterior = () => {
+      //leer el state de la página actual
+      let pagina= this.state.pagina;
+       //si la página es uno ya no ir hacia atrás
+      if(pagina ===1)return null;
+      //sumar uno a la pagina actual
+      pagina -=1;  
+      //agregar el cambio al state
+      this.setState({
+        pagina
+      }, () => {
+          this.consultarApi();
+          this.scroll();
+
+      });
+
+      //console.log(pagina);
+  }
+  paginaSiguiente = () => {
+    //leer el state de la página actual
+    let pagina= this.state.pagina;
+    //sumar uno a la pagina actual
+    pagina +=1;
+    //agregar el cambio al state
+    this.setState({
+      pagina
+    }, () => {
+      this.consultarApi();
+      this.scroll();
+    });
+
+    //console.log(pagina);
   }
 
   consultarApi = () => {
     const termino=this.state.termino;
-    const url = `https://pixabay.com/api/?key=30715967-5ba022496f0ac17e98129ea0d&q=${termino}}&image_type=photo&pretty=true`;
+    const pagina= this.state.pagina;
+
+    const url = `https://pixabay.com/api/?key=30715967-5ba022496f0ac17e98129ea0d&q=${termino}}&image_type=photo&pretty=true&page=${pagina}`;
     fetch(url)
     .then(respuesta => respuesta.json())
     .then(resultado => this.setState({imagenes : resultado.hits}))
@@ -18,9 +60,12 @@ class App extends Component {
 
   datosBusqueda = (termino) => {
       this.setState({
-        termino
-      }, () => {
+        termino: termino,
+        pagina:1     
+    },()=>{
       this.consultarApi();
+      this.scroll();
+
     })
   }
 
@@ -31,11 +76,16 @@ class App extends Component {
           <p className="lead text-center">Buscador de imágenes</p>
           <Buscador 
             datosBusqueda={this.datosBusqueda}
-          />  
-          <Resultado
-           imagenes={this.state.imagenes}
-          />      
+          />             
         </div>
+        <div className='row justify-content-center'>
+            <Resultado
+              imagenes={this.state.imagenes}
+              paginaAnterior={this.paginaAnterior}
+              paginaSiguiente={this.paginaSiguiente}
+
+            />   
+        </div>  
       </div>
     );
   }
