330bdafb7935900dd56a064b68d8a7b7daf69375
VIVIANA RESTREPO QUINTERO <lvrestrepo89570@umanizales.edu.co>
2022-11-18 19:50:43 -0500

    se crea metodo que suma y resta cuando se da anterior o siguiente

--- spaproyectofinal/src/componentes/Paginacion.js
+++ spaproyectofinal/src/componentes/Paginacion.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Paginacion = props => {
     return (
         <div className='py-3'>
+            //botones para pasar de página y devolvernos
             <button onClick={props.paginaAnterior} type='button' className='btn btn-info mr-1'>Anterior &larr;</button>
             <button onClick={props.paginaSiguiente} type='button' className='btn btn-info'>Siguiente &rarr;</button>
         </div>
